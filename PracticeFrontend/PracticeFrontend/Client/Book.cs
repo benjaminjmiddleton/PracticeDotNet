bcec23289a6f2e45d8c176cfb60d48869ac14762
Ben Middleton <bjamesmiddleton@gmail.com>
2024-02-01 11:40:31 -0500

    clean up extra template code

---
+++ PracticeFrontend/PracticeFrontend/Client/Book.cs
@@ -0,0 +1,7 @@
+public class Book
+{
+    public long Id { get; set; }
+    public string? Title { get; set; }
+    public List<string>? Authors { get; set; }
+    public bool CheckedOut { get; set; }
+}
